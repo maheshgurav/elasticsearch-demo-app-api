7327efb6cb6652b4675cd6ab5ec102a23a3f9e49
Tushar Pal <tusharpaldev@gmail.com>
2020-11-18 19:15:51 +0530

    Fixed re-indexing script

--- index_handler.py
+++ index_handler.py
@@ -4,6 +4,8 @@ import argparse
 from argparse import RawTextHelpFormatter
 from logger_config import get_logger
 from elasticsearch import Elasticsearch
+import requests
+import json
 
 logger = get_logger(__name__)
 
@@ -24,10 +26,29 @@ def create_index(index_name):
         logger.error(ex)
 
 
+def reindex(source_index, destination_index):
+    try:
+        reindex_data = {}
+        reindex_data["source"] = {"index" : source_index}
+        reindex_data["dest"] = {"index" : destination_index}
+        response = requests.post("http://localhost:9200/_reindex", data = json.dumps(reindex_data), headers={"Content-Type" : "application/json"})
+    except Exception as ex:
+        logger.error(ex)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         formatter_class=RawTextHelpFormatter, description='Create Indexes...')
-    parser.add_argument('-name', '--index_name',
+    parser.add_argument('-o', '--operation',
+                        type=str, help='Operations can be either of "create" or "reindex"')    
+    parser.add_argument('-n', '--index_name',
                         type=str, help='Index name...')
+    parser.add_argument('-s', '--source_index',
+                        type=str, help='Source index name...')
+    parser.add_argument('-d', '--destination_index',
+                        type=str, help='Destination index name...')
     args = parser.parse_args()
-    create_index(args.index_name)
+    if args.operation == "create":
+        create_index(args.index_name)
+    if args.operation == "reindex":
+        reindex(args.source_index, args.destination_index)
